091b72f4d54541f82d602cf2368d4d1487199668
Amir <amirsol456@gmail.com>
2025-08-29 00:11:56 +0330

    ok

--- Resume.Infrastructure/DBContext/ResumeDbContextFactory.cs
+++ Resume.Infrastructure/DBContext/ResumeDbContextFactory.cs
@@ -8,7 +8,8 @@ namespace Resume.Infrastructure.DBContext
         public ResumeDbContext CreateDbContext(string[] args)
         {
             var optionsBuilder = new DbContextOptionsBuilder<ResumeDbContext>();
-            optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=ResumeDb;Integrated Security=True");
+            //optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=ResumeDb;Integrated Security=True");
+            optionsBuilder.UseSqlServer("Data Source=.;Initial Catalog=ResumeDb;Integrated Security=True;Trust Server Certificate=True");
 
             return new ResumeDbContext(optionsBuilder.Options);
         }
